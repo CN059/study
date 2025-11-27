92cebdc30b5f12af652b141df1527e77a2a1f1c0
CN059 <cn059@outlook.com>
2025-11-27 15:38:08 +0800

    feat: easy use of serde

--- Rust/learn_serde/src/lib.rs
+++ Rust/learn_serde/src/lib.rs
@@ -2,6 +2,14 @@ pub fn add(left: u64, right: u64) -> u64 {
     left + right
 }
 
+/// ## 使用派生宏可以方便的实现序列化和反序列化
+/// 需要在 Cargo.toml 中添加 serde 和 serde_json 依赖，并且启用 serde 的 derive 功能。
+/// ```toml
+/// [dependencies]
+/// serde = { version = "1.0", features = ["derive"] }
+/// serde_json = "1.0"
+/// ```
+/// 
 #[cfg(test)]
 mod tests {
     use super::*;
--- Rust/learn_serde/src/tests/serde_json.rs
+++ Rust/learn_serde/src/tests/serde_json.rs
@@ -31,3 +31,19 @@ fn test_deserialize() {
         }
     );
 }
+
+// serde其实就是一个通用的序列化框架，可以用于多种格式
+// 如果只使用serde,是不能进行我们想要的序列化的
+// 使用其他的依赖于serde的库进行序列化
+#[test]
+fn test_serialize_toml(){
+    let person = Person {
+        name: "Charlie".to_string(),
+        age: 28,
+    };
+    let toml = toml::to_string(&person).unwrap();
+    println!("{}", toml);
+    assert_eq!(toml, r#"name = "Charlie"
+age = 28
+"#);
+}
